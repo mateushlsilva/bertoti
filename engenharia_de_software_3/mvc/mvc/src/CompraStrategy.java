308f93bd013fe922e33d4c423273f49193013545
mateushlsilva <mateushls01@gmail.com>
2023-06-10 18:40:13 -0300

    Adiciona diagrama de class

--- engenharia_de_software_3/mvc/mvc/src/CompraStrategy.java
+++ engenharia_de_software_3/mvc/mvc/src/CompraStrategy.java
@@ -1,6 +1,5 @@
 public class CompraStrategy implements Strategy{
     public void execute(Model model) {
-        // Lógica para comprar ações
         System.out.println("Comprando ações de " + model.getSimbolo() + " por " + model.getPreco());
     }
 }
--- engenharia_de_software_3/mvc/mvc/src/VendaStrategy.java
+++ engenharia_de_software_3/mvc/mvc/src/VendaStrategy.java
@@ -1,6 +1,5 @@
 public class VendaStrategy implements Strategy{
     public void execute(Model model) {
-        // Lógica para vender ações
         System.out.println("Vendendo ações de " + model.getSimbolo() + " por " + model.getPreco());
     }
 }
